b495c1a2749bfc331dffc04eb816e33aa8c28f47
Gema <gem.anggada@gmail.com>
2018-08-23 14:47:15 +0700

    add prisma binding

--- src/index.js
+++ src/index.js
@@ -2,84 +2,45 @@
 
 import {GraphQLServer} from 'graphql-yoga';
 import schema from './schema.graphql';
-
-type Link = {
-  id: string;
-  url: string;
-  description: string;
-};
-
-type Links = Array<Link>;
-
-let links: Links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL',
-  },
-  {
-    id: 'link-1',
-    url: 'www.facebook.com',
-    description: 'Facebook, a website for procrastinator',
-  },
-  {
-    id: 'link-2',
-    url: 'www.twitter.com',
-    description: 'site for gossip',
-  },
-];
-
-let idCount: () => number = () => links.length;
+import {Prisma} from 'prisma-binding';
 
 const resolvers = {
   Query: {
-    info: () => 'hackernews clone server',
-    feed: () => links,
-    link: (root, args) => links.find((link) => link.id === args.id),
+    info: () => 'Basic graphql server',
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    },
   },
-
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount()}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description,
+          },
+        },
+        info
+      );
     },
-    updateLink: (root, args) => {
-      let {id, url, description} = args;
-      let newLink = {
-        id,
-        url,
-        description,
-      };
-      links = links.map((link) => {
-        if (link.id === id) {
-          return newLink;
-        }
-        return link;
-      });
-      return newLink;
-    },
-    deleteLink: (root, args) => {
-      let deletedLink = links.find((link) => link.id === args.id);
-      links = links.filter((link) => link.id !== args.id);
-      return deletedLink;
-    },
-  },
-
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
   },
 };
 
 const server = new GraphQLServer({
   typeDefs: schema,
   resolvers,
+  context: (req) =>
+    Object.assign(
+      {
+        db: new Prisma({
+          typeDefs: 'src/generated/prisma.graphql',
+          endpoint: 'https://us1.prisma.sh/gema-anggada-c8a576/database/dev',
+          secret: 'mysecret123',
+          debug: true,
+        }),
+      },
+      req
+    ),
 });
 
 server.start(() => console.log('server is running on http://localhost:4000'));
